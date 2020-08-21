a6b6880900ce46ad5dddc1a5598eb1a95c2aaba4
MatheusAL <matheus1999lima@hotmail.com>
2020-08-21 20:35:58 -0300

    form att

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -1,7 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import SlickSlider from 'react-slick'
 import styled from 'styled-components';
-
 
 const Container = styled.ul`
   padding: 0;
@@ -38,12 +38,11 @@ export const SliderItem = styled.li`
   }
 `;
 
-
 const Slider = ({ children }) => (
   <Container>
     <SlickSlider {...{
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 300,
       centerMode: false,
       variableWidth: true,
@@ -55,4 +54,4 @@ const Slider = ({ children }) => (
   </Container>
 );
 
-export default Slider; 
+export default Slider;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,98 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
 
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
 
+  const [categorias, setCategorias] = useState([]);
 
+  useEffect(() => {
+    const url = 'http://localhost:3000/categorias';
+    fetch(url)
+      .then(async (respostadoServidor) => {
+        const resposta = await respostadoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  });
 
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        clearForm();
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      {categorias.length === 0 && (
+      <div>
+        {/* Cargando... */}
+        Loading...
+      </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
